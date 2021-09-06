4c87e316eb48ab0bb27d6681e9f93702503f7ee8
xFixMeme <86025073+xFixMeme@users.noreply.github.com>
2021-09-06 16:04:12 +0200

    Add files via upload

---
+++ attacks/proxy.js
@@ -0,0 +1,21 @@
+const https = require('https');
+const fs = require('fs');
+const Discord = require("discord.js");
+const moment = require('moment');
+
+module.exports.run = async (bot, message, args) => {
+    var url = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all"
+    const file = fs.createWriteStream("socks4.txt")
+    fs.writeFileSync('socks4.txt', ' ');
+    const request = https.get(url, function(response) {
+        response.pipe(file)
+    });
+
+    message.channel.send("[*] Proxy updated!")
+}
+
+
+module.exports.help = {
+    name: "update",
+    aliases: ["upd"]
+}
